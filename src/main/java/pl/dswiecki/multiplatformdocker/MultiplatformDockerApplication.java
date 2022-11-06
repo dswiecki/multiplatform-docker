3770a1a676be0f7b6a775f6e4317aceb84c829f8
Damian Święcki <dswiecki9@gmail.com>
2022-11-06 13:21:32 +0100

    Initial commit

---
+++ src/main/java/pl/dswiecki/multiplatformdocker/MultiplatformDockerApplication.java
@@ -0,0 +1,13 @@
+package pl.dswiecki.multiplatformdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplatformDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplatformDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dswiecki/multiplatformdocker/MultiplatformDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dswiecki.multiplatformdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplatformDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
